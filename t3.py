826506b2b191707681abb4b27a331af818393955
viraj-poolabhavi <virajp0918@gmail.com>
2026-02-11 15:45:10 +0530

    Change the file

--- t3.py
+++ t3.py
@@ -1,6 +1,6 @@
 print('Hii this is from main!!')
 
-print('This is from the demo branch!!')
+print('This is the change that i want to see it in the git difference! That is being made for another changes to see!!')
 
 
 print('This is the first line of the test file that are required !!!!')
--- test1.py
+++ test1.py
@@ -1,8 +1,4 @@
 print('This is all the test file that are required !!!!')
-
-
-print('This is the second line of the test file that are required !!!!')
-
-print('This is the third line of the test file that are required !!!!')
+print('Added the line to see multiple file difference once it has been staged')
 
 #Hi this is a new commit that I am doing to test the git commands and the github repository
